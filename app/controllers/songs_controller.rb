3442b0e3122d23377b7ff1b8abf9c498e5e34db4
leduc1901 <letrongduc1999@gmail.com>
2020-06-24 16:36:46 +0700

    livesearch

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,17 @@
 class SongsController < ApplicationController
-    before_action :authorize_request
+    before_action :authorize_request , except: :search
     def index 
         @songs = Song.all.includes(:category, {song_url_attachment: :blob} , singer: {image_attachment: :blob} )
         render json: @songs.map{|song| {id: song.id , name: song.name , category: song.category.name , image: rails_blob_url(song.singer.image, only_path: true), singer: song.singer.name , url: rails_blob_url(song.song_url, only_path: true)}}, status: :ok
     end
+
+    def search 
+        @parameter = params[:search]
+        
+        @songs = Song.all.includes(:category, {song_url_attachment: :blob} , singer: {image_attachment: :blob} )
+        @songs = @songs.left_outer_joins(:category , :singer).where("songs.name LIKE ? OR categories.name LIKE ? OR singers.name LIKE ?", "%#{@parameter}%" , "%#{@parameter}%" , "%#{@parameter}%")
+        render json: @songs.map{|song| {id: song.id , name: song.name , category: song.category.name , image: rails_blob_url(song.singer.image, only_path: true), singer: song.singer.name , url: rails_blob_url(song.song_url, only_path: true)}}, status: :ok
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :categories , param: :slug
   get "/playlists/user/:id" , to: 'playlists#show_from_user'
   post 'auth/login' , to: 'authentication#login'
+  post "search/:search" => "songs#search" 
   # get "/*a" , to: 'application#not_found'
 end
